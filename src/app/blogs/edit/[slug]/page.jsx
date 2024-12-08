7135c13a3a964b82ad0680553f42166547e5ef5f
rnrran <randykamalhusen@gmail.com>
2024-12-08 23:51:28 +0800

    hadeh masih belum bisa buat ngatur abilitinya

--- src/app/blogs/edit/[slug]/page.jsx
+++ src/app/blogs/edit/[slug]/page.jsx
@@ -1,8 +1,8 @@
 'use client'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useRouter } from 'next/navigation';
 import Swal from 'sweetalert2'; 
-
+import { AuthContext } from '../../../../context/AuthContext';
 const EditBlog = ({ params }) => {
   const { slug } = React.use(params) 
   const [blog, setBlog] = useState(null);
@@ -12,28 +12,44 @@ const EditBlog = ({ params }) => {
   const [description, setDescription] = useState('');
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(false);
+  const {currentUser, userData} = useContext(AuthContext);
+  const {userRole, setUserRole} = useState('')
   // const [blogID, setBlogID] = useState('');
   const router = useRouter();
+
 
   // Fetch blog data based on slug
   useEffect(() => {
     if (slug) {
       fetchBlogData(slug);
     }
-  }, [slug]);
+  }, [slug, userData]);
 
   const fetchBlogData = async (slug) => {
     try {
       const res = await fetch(`http://localhost:8000/api/blogs/${slug}`)
       const data = await res.json();
-      // console.log("data", data?.id)
-      
+      // setUserRole(userData?.role)
+      // Cek apakah user yang sedang login adalah admin atau author dari blog ini
       setBlog(data);
-      // setBlogID(data?.id);
-      setTitle(data.title);
-      setContent(data.content);
-      setCategoryId(data.category_id);
-      setDescription(data.description);
+      console.log("haha:",data?.user_id)
+      // if (userData?.role == 'admin' || data?.user_id == userData?.id) {
+          // setBlogID(data?.id);
+        setTitle(data.title);
+        setContent(data.content);
+        setCategoryId(data.category_id);
+        setDescription(data.description);
+      // }
+      // else {
+        Swal.fire({
+          title: 'Akses Ditolak',
+          text: 'Kamu bukan admin atau author dari tulisan ini!',
+          icon: 'error',
+        });
+        router.push('/blogs'); // Redirect jika akses tidak diperbolehkan
+        return; // Keluar dari fungsi jika akses ditolak
+      // }
+      
     } catch (error) {
       console.error('Error fetching blog data:', error);
     }
@@ -65,57 +81,69 @@ const EditBlog = ({ params }) => {
       category_id: categoryId,
       description,
     };
+    console.log(userData?.role, blog)
+    // if (userData?.role !== 'admin' || blog?.user_id !== userData.id) {
+    //   // Jika bukan admin atau bukan author, tampilkan pesan
+    //   Swal.fire({
+    //     title: 'Akses Ditolak',
+    //     text: 'Kamu bukan admin atau author dari tulisan ini!',
+    //     icon: 'error',
+    //   });
+    //   router.push('/blogs');
+    // }
 
-    const result = await Swal.fire({
-      title: 'oke ?',
-      text: "perbarui tulisan kakmu ?",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Ya, update!',
-      cancelButtonText: 'Batal',
-    });
-
-
-    if (result.isConfirmed) {
-    try {
-      const res = await fetch(`http://localhost:8000/api/blogs/${blog.id}`, {
-        method: 'PUT',
-        headers: {
-          'Authorization': `Bearer ${localStorage.getItem('user_token')}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedBlog),
+    // else {
+      const result = await Swal.fire({
+        title: 'oke ?',
+        text: "perbarui tulisan kakmu ?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Ya, update!',
+        cancelButtonText: 'Batal',
       });
 
-      const data = await res.json();
 
-      if (res.ok) {
-        await Swal.fire({
-          title: 'Blog Updated!',
-          text: 'Your blog has been updated successfully.',
-          icon: 'success',
-        });
-        router.push(`/blogs/${data?.id}`);
+      if (result.isConfirmed) {
+        try {
+          const res = await fetch(`http://localhost:8000/api/blogs/${blog.id}`, {
+            method: 'PUT',
+            headers: {
+              'Authorization': `Bearer ${localStorage.getItem('user_token')}`,
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedBlog),
+          });
+
+          const data = await res.json();
+
+          if (res.ok) {
+            await Swal.fire({
+              title: 'Blog Updated!',
+              text: 'Your blog has been updated successfully.',
+              icon: 'success',
+            });
+            router.push(`/blogs/${data?.id}`);
+          } else {
+            Swal.fire({
+              title: 'Error',
+              text: data.message || 'Failed to update blog.',
+              icon: 'error',
+            });
+          }
+        } catch (error) {
+          console.error('Error updating blog:', error);
+          Swal.fire({
+            title: 'Error',
+            text: 'Something went wrong while updating the blog.',
+            icon: 'error',
+          });
+        } finally {
+          setLoading(false);
+        }
       } else {
-        Swal.fire({
-          title: 'Error',
-          text: data.message || 'Failed to update blog.',
-          icon: 'error',
-        });
+        setLoading(false); 
       }
-    } catch (error) {
-      console.error('Error updating blog:', error);
-      Swal.fire({
-        title: 'Error',
-        text: 'Something went wrong while updating the blog.',
-        icon: 'error',
-      });
-    } finally {
-      setLoading(false);
-    }
-  } else {
-    setLoading(false); 
-  }
+    // }
   };
 
   if (!blog) {
